2547d41d54ca3c0e8d8d5c1912a399636261e48e
Fr33styler <Fr33stylerMC@gmail.com>
2023-07-24 17:28:53 +0300

    Small code refactor

--- src/main/java/ro/fr33styler/groupedworldchat/listeners/ChatEvent.java
+++ src/main/java/ro/fr33styler/groupedworldchat/listeners/ChatEvent.java
@@ -1,5 +1,6 @@
 package ro.fr33styler.groupedworldchat.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -16,20 +17,21 @@ public class ChatEvent implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGH)
-    public void onChat(AsyncPlayerChatEvent e) {
-        Group group = plugin.getGroups().get(e.getPlayer().getWorld().getUID());
+    public void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        Group group = plugin.getGroups().get(player.getWorld().getUID());
         if (group != null) {
             String format = group.getFormat();
             if (plugin.getPapi() != null) {
-                format = plugin.getPapi().replace(e.getPlayer(), format);
+                format = plugin.getPapi().replace(player, format);
             }
             try {
-                e.setFormat(format);
+                event.setFormat(format);
             } catch (Exception ignored) {
                 plugin.getLogger().severe("The format couldn't be parsed: " + format);
-                e.setCancelled(true);
+                event.setCancelled(true);
             }
-            e.getRecipients().removeIf(p -> !(p.hasPermission("gwc.admin") || plugin.getGroups().get(p.getWorld().getUID()) == group));
+            event.getRecipients().removeIf(recipient -> !(recipient.hasPermission("gwc.admin") || plugin.getGroups().get(recipient.getWorld().getUID()) == group));
         }
     }
 
